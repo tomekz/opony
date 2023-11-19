7727e91b09097e1b09c1d72fb5937d516d80e26c
tomekz <zadrozny.tomasz@gmail.com>
2023-11-19 18:12:34 +0100

    fix: pass sheetOptions

--- app/api/opony/route.ts
+++ app/api/opony/route.ts
@@ -166,10 +166,9 @@ export async function POST(request: NextRequest) {
                     }
 
                     const output = augmentData(input, results);
-                    
                     console.log("augmented data", {output});
-                    const options = {'!cols': [{wch: 5}, {wch: 5}, {wch: 10}, {wch: 20}]};
-                    const buffer = xlsx.build([{name: "wyniki", data: output, options }]);
+                    const sheetOptions = {'!cols': [{wch: 5}, {wch: 5}, {wch: 10}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}, {wch: 20}]};
+                    const buffer = xlsx.build([{name: "wyniki", data: output}], {sheetOptions});
                     const blob = await put("wyniki_" + Date.now().toString() + ".xls", buffer, { access: 'public' });
                     resolve(blob);
                 });
